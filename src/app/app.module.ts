a287e0379ca6b6f78e95b451aa01937000c9528e
Manuel <manuelsapology@gmail.com>
2019-02-25 00:12:56 -0400

    comienzo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
+import { ROUTES } from './app.routes';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { NavbarreComponent } from './components/shared/navbarre/navbarre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    AboutComponent,
+    PortfolioComponent,
+    ContactComponent,
+    FooterComponent,
+    NavbarreComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES, {useHash: true}),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/shared/navbarre/navbarre.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbarre',
+  templateUrl: './navbarre.component.html',
+  styleUrls: ['./navbarre.component.scss']
+})
+export class NavbarreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
