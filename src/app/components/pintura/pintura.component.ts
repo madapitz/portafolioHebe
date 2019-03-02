d0341d448edb2f28789c1920601016c7f7adb9a4
Manuel <manuelsapology@gmail.com>
2019-03-02 03:24:56 -0400

    modal para las imagenes del portafolio

--- src/app/components/pintura/pintura.component.ts
+++ src/app/components/pintura/pintura.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class PinturaComponent implements OnInit {
 
   @Input() nombre:string;
+  @Input('index') index:number;
 
   constructor() { }
 
